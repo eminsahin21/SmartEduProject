d305cb62241709fe64bd900b58644958b364796c
Mehmet Emin Şahin <mehmeteminsahin34@gmail.com>
2024-04-09 18:32:36 +0300

    kurs modeli olusturuldu

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require("express");
+const mongoose = require("mongoose")
+const pageRoute = require("./routes/pageRoutes")
+const courseRoute = require("./routes/courseRoute")
 
 const app = express();
+
+//connect database
+mongoose.connect('mongodb://127.0.0.1/smartedu-db',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+    console.log("Veritabanına başarılı bir şekilde bağlanıldı.")
+});
 
 //TEMPLATE ENGINE
 app.set('view engine','ejs')
 
 //Middlewares
 app.use(express.static("public"))
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
-app.get("/", (req, res) => {
-  res.status(200).render("index",{
-    page_name:'index'
-  });
-});
-
-app.get("/about", (req, res) => {
-    res.status(200).render("about",{
-        page_name:'about'
-    });
-  });
+//Routes
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 const port = 3000;
 app.listen(port, () => {
